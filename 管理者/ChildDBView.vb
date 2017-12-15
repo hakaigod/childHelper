77e6be704b769c37fff138458e3d89224f77a54a
hyarapicoru <hyarapicoru@yahoo.co.jp>
2017-12-15 14:21:36 +0900

    admin

--- 管理者/ChildDBView.vb
+++ 管理者/ChildDBView.vb
@@ -5,7 +5,7 @@
     End Sub
 
     Private Sub BunifuGradientPanel1_Paint(sender As Object, e As PaintEventArgs)
-        ''
+        '
     End Sub
 
     Private Sub TableLayoutPanel3_Paint(sender As Object, e As PaintEventArgs)
